1da5ea8eb46a8ec75d6f5475491a492a53257a06
cesar <cesar@PC-CESAR.home>
2013-11-06 16:26:30 -0200

    Adicionada classe para testar o jasperReport. Nessa classe criarei o PDF do relatorio.

---
+++ src/menu/financeiro/relatorios/atendimentos/iReport/todosOsConvenios.java
@@ -0,0 +1,14 @@
+package menu.financeiro.relatorios.atendimentos.iReport;
+
+public class todosOsConvenios {
+
+    public static void main(String[] args) {
+        
+
+        //nesta classe testaremos o jasperReport
+        //ainda estou assistindo as aulas!
+        //mas trabalharei somente com esta classe para testar
+
+    }
+
+}
