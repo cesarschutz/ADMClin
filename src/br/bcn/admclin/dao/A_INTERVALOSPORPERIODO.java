813dce22bf3c920c950cb91e988a0d3fc7af6c49
Cesar Schutz <cesarschutz340@gmail.com>
2013-11-21 16:00:36 -0200

    Deletada classe intervalosPorPeriodoDAO do pacote enu.atendimentos.agenda.dao *colocado o metodo na classe _INTERVALOSPORPERIODO *arrumada classe que utiliza este metodo

--- src/br/bcn/admclin/dao/A_INTERVALOSPORPERIODO.java
+++ src/br/bcn/admclin/dao/A_INTERVALOSPORPERIODO.java
@@ -35,6 +35,25 @@ public class A_INTERVALOSPORPERIODO {
             return resultSet;
         }
     }
+    
+    /**
+     * Consulta Todos os intervalos por periodo existentes no Banco de Dados  de uma agenda
+     * @param Connection
+     * @return ResultSet
+     */
+    public static ResultSet getConsultarIntervalosPorPeriodoPorAgenda(Connection con, int agendaId){
+        ResultSet resultSet = null;
+        try{
+        PreparedStatement stmtQuery = con.prepareStatement("select A_INTERVALOSPORPERIODO.agendaid, A_INTERVALOSPORPERIODO.A_INTERVALOPORPERIODONID, A_INTERVALOSPORPERIODON.A_INTERVALOPORPERIODONID, A_INTERVALOSPORPERIODON.HORARIOINICIAL, "
+                + "A_INTERVALOSPORPERIODON.HORARIOFINAL, A_INTERVALOSPORPERIODON.DIAINICIAL, A_INTERVALOSPORPERIODON.DIAFINAL "
+                + "from A_INTERVALOSPORPERIODO inner join A_INTERVALOSPORPERIODON on A_INTERVALOSPORPERIODO.A_INTERVALOPORPERIODONID = A_INTERVALOSPORPERIODON.A_INTERVALOPORPERIODONID where A_INTERVALOSPORPERIODO.agendaid ="+agendaId);
+        resultSet = stmtQuery.executeQuery();
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao verificar os Intervalos por Período. Procure o Administrador.","ERRO",javax.swing.JOptionPane.ERROR_MESSAGE);
+        }finally{
+            return resultSet;
+        }
+    }
         
     
     /**
